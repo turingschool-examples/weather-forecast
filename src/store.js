dc0f93ef7cff960455053a7ad98e1b0d263b3863
kylem038 <misenckj@gmail.com>
2016-10-06 16:02:04 -0600

    Added hot reload for reducers

--- src/store.js
+++ src/store.js
@@ -3,9 +3,19 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 const middleware = [thunk, createLogger];
 import rootReducer from './reducers';
+import currentWeather from './data/fake-current';
 
 const store = createStore(rootReducer,
   applyMiddleware(...middleware)
 );
+
+
+
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default store;
