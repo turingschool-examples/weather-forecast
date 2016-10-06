3fb24571b6eb35367eeefae46d98a706f004dab1
Brett Hevia <brett.hevia@gmail.com>
2016-10-06 11:37:34 -0600

    inital router setup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// import { Link, Router, IndexRoute, Route, browserHistory } from 'react-router';
 
 class App extends Component {
   render() {
     return (
-      <div>Hey!</div>
+      <section>
+       <div>{this.props.children}</div>
+     </section>
     )
   }
 }
 
 const mapStateToProps = state => {
-  // return an object of redux store data
-  // that you'd like available in your component
+
   return {};
 }
 
