ee952c611d6c9d25d068c58da659e0d69bf0c113
Brett Hevia <brett.hevia@gmail.com>
2016-10-08 15:28:44 -0600

    styled header

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-export default class ExtendedForecast extends Component {
+
+export default class Settings extends Component {
   render() {
     return (
-      <section id="ExtendedForecast">
-        <h1>Extended Forecast</h1>
-        <Link to="/Settings">Go to pinned cities</Link>
+      <section id="Settings">
+        <h1>PinnedCities</h1>
+        <Link to="/ExtendedForecast">Go to Extended Forecast</Link>
         <Link to="/">Go to HomePage</Link>
       </section>
     )
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { IndexRoute, Router, Route  } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import PinnedCities from './containers/PinnedCities';
+import PinnedCities from './containers/Settings';
 import ExtendedForecast from './containers/ExtendedForecast';
 
 const Routes = (props) => (
@@ -10,7 +10,7 @@ const Routes = (props) => (
     <Route path="/" component={App}>
       <IndexRoute component={HomePage} />
       <Route path="/ExtendedForecast" component={ExtendedForecast} />
-      <Route path="/PinnedCities" component={PinnedCities} />
+      <Route path="/Settings" component={PinnedCities} />
     </Route>
   </Router>
 );
