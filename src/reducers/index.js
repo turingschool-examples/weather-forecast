62e661ae4dd9eae4690ebec4604dba0b3cb56363
kylem038 <misenckj@gmail.com>
2016-10-09 13:31:11 -0600

    All components should be dumb now

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import settings from './settings';
 import extendedForecast from './extended-forecast';
 import fiveday from './fiveday';
+import header from './header';
 
 const rootReducer = combineReducers({
   settings,
   extendedForecast,
-  fiveday
+  fiveday,
+  header
 });
 
 export default rootReducer;
