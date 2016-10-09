ff47508a2a6ee9c66a0f78be720e7efed05271e2
Brett Hevia <brett.hevia@gmail.com>
2016-10-08 21:32:24 -0600

    fix open string in header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return(
       <section id="header">
-        <p id="current-local-forecast">Current forecast for <span id="city-name">Mike's Butt</span></p>
+        <p id="current-local-forecast">Current forecast for <span id="city-name">Mikes Butt</span></p>
         <p id="view-extended-forecast">Lots and lots of poo-poo</p>
       </section>
     )
