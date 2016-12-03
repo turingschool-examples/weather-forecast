bdb6dabacb54959b69bc77b3f3826c3a30af7481
Andrew Crist <andrew.l.crist@gmail.com>
2016-12-03 15:17:05 -0700

    Fixed lower case

--- src/containers/PinnedWeatherContainer.js
+++ src/containers/PinnedWeatherContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PinnedWeather from '../components/PinnedWeather';
-import App from '../components/App';
+import App from '../components/app';
 import { fetchWeatherPinnedLocation } from '../actions/index'
 
 
