6201cfcc1cb31a5eafa9731cc627cd59b7fe15a9
ccgamble <christine.c.gamble@gmail.com>
2016-12-05 12:53:38 -0700

    2 tests still failing

--- tests/components/pinnedlocation.test.js
+++ tests/components/pinnedlocation.test.js
@@ -5,7 +5,7 @@ import PinnedWeather from '../../src/components/PinnedWeather';
 
 describe('PinnedWeather', () => {
 
-  it('has the id of header', () => {
+  it('has the className of pinned-weather', () => {
     const wrapper = shallow(<PinnedWeather className='pinned-weather' />);
 
     expect(wrapper.is('.pinned-weather')).toEqual(true);
