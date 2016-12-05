6d0b928cd7359daa2c07ab75e57974c2626e5594
Andrew Crist <andrew.l.crist@gmail.com>
2016-12-05 10:23:07 -0700

    Refactor

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,7 +17,7 @@ export const fetchWeatherCurrentLocation = (position) => {
 
 	return (dispatch) => {
 
-		return fetch(`https://api.wunderground.com/api/881631f063e09bd3/conditions/forecast10day/alerts/hourly10day/q/${latitude},${longitude}.json`)
+		return fetch(`https://api.wunderground.com/api/f9433085853dff39/conditions/forecast10day/alerts/hourly10day/q/${latitude},${longitude}.json`)
 		.then(weather => weather.json())
  		.then((weatherInfo) => {
 	 		dispatch({type: 'SET_LOCAL_WEATHER', weatherInfo})
@@ -33,34 +33,3 @@ export const fetchWeatherPinnedLocation = ( zip ) => {
 			.catch(error => console.log(error))
   	}
 };
-
-//
-// export const fetchWeatherPinnedLocation = (zip) => {
-// 	return (dispatch) => {
-//
-// 		return axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${apiKey}&units=imperial`)
-// 			.then(weatherInfo => dispatch({ type: 'SET_PINNED_WEATHER', weatherInfo }))
-// 		  .catch(error => console.log(error));
-// 		}
-// 	}
-//
-// export const pinLocation = (zip) => {
-// 	return (dispatch) => {
-// 		return axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${apiKey}&units=imperial`)
-// 			.then((weatherInfo) => dispatch({ type: 'PIN_LOCATION', weatherInfo }))
-// 		  .catch(error => console.log(error))
-// 		}
-// 	}
-//
-// export const fetchExtendedLocation = (position) => {
-// 	const latitude = position.coords.latitude;
-//  	const longitude = position.coords.longitude;
-//
-// 	return (dispatch) => {
-// 		return fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${apiKey}&units=imperial`)
-// 		.then(weather => weather.json())
-//  		.then((weatherInfo) => {
-// 	 		dispatch({type: 'SET_EXTENDED_WEATHER', weatherInfo})
-// 		})
-// 	}
-// }
