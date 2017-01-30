11bcaa33531e2d120b8799813e7b54fcbf8c3612
Noah Peden <noahpeden@gmail.com>
2017-01-30 16:45:52 -0700

    getting current location

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <Header />
-  )
+class App extends Component {
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition((position) => {
+         console.log(position.coords.latitude, position.coords.longitude);
+       });
+  }
+  render(){
+    return (
+      <Header />
+    )
+  }
 }
+
+
 
 export default App;
