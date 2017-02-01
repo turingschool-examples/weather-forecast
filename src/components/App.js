99edda2aa07f613f8f66df104ab0f7c3a8e636c0
Noah Peden <noahpeden@gmail.com>
2017-02-01 15:00:16 -0700

    finally figured out correct url stuff

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,44 @@ class App extends Component {
 
   getCurrentLocation(){
     navigator.geolocation.getCurrentPosition((data) => {
-       this.props.fetchLocation(data)
-      // console.log(data)
-    }).then(()=>{
-      return this.getWeather()
+       this.props.fetchLocation(data).then(()=>{
+         return this.getWeather()
+       })
     })
   }
 
-  getWeather(){
-    console.log('pizzarat')
+  getWeather(weatherURL){
+    fetch(weatherURL)
+    .then((response) => {
+      return response.json() })
+    .then((json) => {
+      console.log(json) })
+    .catch((error) => {
+      console.log(error)
+    })  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const weatherURL = `http://api.wunderground.com/api/6fc8de6a49f48b06/geolookup/forecast/hourly/forecast10day/q/${position.coords.latitude},${position.coords.longitude}.json`
+
+      this.getWeather(weatherURL)
+
+
+
+        return(
+          <div>
+            "Stuff"
+          </div>
+        )
+      })
+    }
+
+
+    render(){
+      return (
+        <Header />
+      )
+    }
   }
 
-  componentDidMount(){
-    this.getCurrentLocation()
-  }
-  render(){
-    return (
-      <div>
-      <Header />
-      </div>
-    )
-  }
-}
 export default App;
