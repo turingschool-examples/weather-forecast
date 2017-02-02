5e9dfbd4362ee9b21c0106672f11a51612b5014f
DanGrund <hello@dangrund.com>
2017-02-02 14:11:08 -0700

    holy shit its daytime

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import Header from './Header';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import HeaderContainer from '../containers/HeaderContainer'
 
 class App extends Component {
@@ -24,9 +24,10 @@ class App extends Component {
     .then((json) => {
       this.props.fetchSunriseSunset(json)})
     .then(()=>{
-      console.log(this.props.sunrise.sunset)
-      console.log(moment.utc().format('h:mm:ss a').toUpperCase())
-      console.log(this.props.sunrise.sunrise)
+      let sunrise = moment.parseZone(`${this.props.sunrise.sunrise}`).local().format('HH:mm:ss')
+      let now = moment().format('HH:mm:ss')
+      let sunset = moment.parseZone(`${this.props.sunrise.sunset}`).local().format('HH:mm:ss')
+      if (now > sunrise && now < sunset){console.log("it's fuckin' daytime baby!")}
     })
   }
 
@@ -39,7 +40,7 @@ class App extends Component {
     navigator.geolocation.getCurrentPosition((position) => {
       const weatherURL = `http://api.wunderground.com/api/6fc8de6a49f48b06/geolookup/forecast/hourly/forecast10day/conditions/q/${position.coords.latitude},${position.coords.longitude}.json`
 
-      const sunriseURL = `http://api.sunrise-sunset.org/json?lat=${position.coords.latitude}&lng=${position.coords.longitude}`
+      const sunriseURL = `http://api.sunrise-sunset.org/json?lat=${position.coords.latitude}&lng=${position.coords.longitude}&formatted=0`
 
       this.getWeather(weatherURL)
       this.getSunrise(sunriseURL)
@@ -53,9 +54,7 @@ class App extends Component {
     render(){
       return (
         <div>
-
           <HeaderContainer />
-          {this.props.sunrise ? <div>{this.props.sunrise.sunset} {this.props.sunrise.sunrise}</div> : <div>oh no</div>}
       </div>
       )
     }
