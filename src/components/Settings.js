a2b0685b6866ef85d0898fe1bc410e0bd3c6b17c
Noah Peden <noahpeden@gmail.com>
2017-02-03 16:24:11 -0700

    settings is set up with input

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react';
 import {Link} from 'react-router'
 
-const Settings = (props) => {
-
- const cities = props.cityWeather.map((city, index)=>{
-      return (
-        <div className='city-card'>
-          <ul>
-            <button onClick={() => props.removeClickedCity(index)}>X</button> City: {city.city}
-          </ul>
-        </div>
-      )
-    })
+class Settings extends Component {
+constructor(){
+  super()
+  this.state = {
+    zip: ''
+  }
+}
+render(){
+    const cities = this.props.cityWeather.map((city, index)=>{
+         return (
+           <div className='city-card'>
+             <ul>
+               <button onClick={() => this.props.removeClickedCity(index)}>X</button> City: {city.city}
+             </ul>
+           </div>
+         )
+       })
+    const pinCity = () => {
+      const weatherURL = `http://api.wunderground.com/api/6fc8de6a49f48b06/geolookup/forecast/hourly/forecast10day/conditions/q/${this.state.zip}.json`
+      fetch(weatherURL)
+      .then((response) => {
+        return response.json() })
+      .then((json) => {
+        this.props.getPinned(json) })
+      .catch((error) => {
+        console.log(error)
+      })
+    if(this.props.cityWeather.length >= 3){
+      this.props.cityWeather.shift()
+      }
+    }
     return(
       <div>
-        {cities}
+        <input value={this.state.zip} onChange={e => this.setState({zip: e.target.value})} placeholder="zip code" />
+        <button onClick={() => pinCity()}>Pin New City</button>
+        <div className='city-card'>{cities}</div>
         <Link to="/">
-          <button>Get Back, Get Back</button>
+          <button>Home</button>
         </Link>
       </div>
-    )
+    )}
 }
 export default Settings
